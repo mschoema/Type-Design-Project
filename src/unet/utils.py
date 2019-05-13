59f933d439932ef921d4b3adb9c3fae3749c8824
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2019-05-13 11:56:39 +0200

    solve small bug

--- src/unet/utils.py
+++ src/unet/utils.py
@@ -56,7 +56,7 @@ def scale_back(images):
 def merge(images, size):
     h, w = images.shape[1], images.shape[2]
     img = np.zeros((h * size[0], w * size[1], 1))
-    for idx, image in enumerate(a):
+    for idx, image in enumerate(images):
         i = idx % size[1]
         j = idx // size[1]
         img[j * h:j * h + h, i * w:i * w + w, :] = image
