8c171256b07d1241d9362e2f1ac9c30c4dd0bf46
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2019-05-17 16:06:15 +0200

    change model names (need to use experiment number now to distinguish)

--- src/unet/unet.py
+++ src/unet/unet.py
@@ -206,14 +206,12 @@ class UNet(object):
         return input_handle, loss_handle, eval_handle, summary_handle
 
     def get_model_id_and_dir(self):
-        model_id = "l1_loss_%d_const_loss_%d_experiment_%d_batch_%d" % (self.L1_penalty, self.Lconst_penalty, self.experiment_id, self.batch_size)
-        model_in_out_type = "%s_input_%s_target" % (self.input_type, self.target_type)
-        model_dir_start = os.path.join(self.checkpoint_dir, model_in_out_type)
-        model_dir = os.path.join(model_dir_start, model_id)
+        model_id = "experiment_%d_batch_%d" % (self.L1_penalty, self.Lconst_penalty, self.experiment_id, self.batch_size)
+        model_dir = os.path.join(self.checkpoint_dir, model_id)
         return model_id, model_dir
 
     def checkpoint(self, saver, step):
-        model_name = "l1_loss_%d_const_loss_%d_unet.model" % (self.L1_penalty, self.Lconst_penalty)
+        model_name = "unet.model" % (self.L1_penalty, self.Lconst_penalty)
         model_id, model_dir = self.get_model_id_and_dir()
 
         if not os.path.exists(model_dir):
